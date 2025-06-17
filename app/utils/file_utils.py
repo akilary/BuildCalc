e772d744d3c48942ae5da78d9355e0783296e65c
akiry <a.akilary@gmail.com>
2025-06-17 23:27:32 +0500

    баг фиксы с порядком шагов

--- app/utils/file_utils.py
+++ app/utils/file_utils.py
@@ -3,7 +3,9 @@ import os
 
 def get_calc_steps() -> list[str]:
     """Возвращает список шагов калькулятора на основе имеющихся шаблонов"""
-    return [
-        f for f in os.listdir("app/templates/calculator/steps")
-        if not (f.startswith("~") or f.endswith("~") or f.startswith("."))
-    ]
+    return sorted(
+        [
+            f for f in os.listdir("app/templates/calculator/steps")
+            if not (f.startswith("~") or f.endswith("~") or f.startswith("."))
+        ]
+    )
